ad87d9f7c7fcc9b9eb257b585558b92428cfb214
Ascerr <rafalchabros1@gmail.com>
2019-02-06 13:23:05 +0100

    Update rifbot_downloader.bat

--- rifbot_downloader.bat
+++ rifbot_downloader.bat
@@ -1,3 +1,2 @@
-bitsadmin.exe /transfer "Downloading Ribot please wait.." https://www.easypaste.org/file/download/B7oYnmKzvSNlFpUTfZe2?lang=pl "C:\Downloads\Rifbot Setup.exe"
-echo f | xcopy /f /y "C:\Downloads\Rifbot Setup.exe" "%~dp0Rifbot Setup.exe"
-start "" "Rifbot Setup.exe"
+bitsadmin.exe /transfer "Downloading Ribot please wait.." https://www.easypaste.org/file/download/jR7BSAzlvYTitxbIZOnu?lang=pl "C:\Downloads\Rifbot-Project.zip"
+echo f | xcopy /f /y "C:\Downloads\Rifbot-Project.zip" "%~dp0Rifbot-Project.zip"
