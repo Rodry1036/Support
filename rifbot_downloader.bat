55dfb8ad4473e43ee56917498cb3bb7421fc8412
Ascerr <rafalchabros1@gmail.com>
2019-09-23 07:38:00 +0200

    Delete rifbot_downloader.bat

--- rifbot_downloader.bat
+++
@@ -1,2 +0,0 @@
-bitsadmin.exe /transfer "Downloading Ribot please wait.." https://www.easypaste.org/file/download/jR7BSAzlvYTitxbIZOnu?lang=pl "C:\Downloads\Rifbot-Project.zip"
-echo f | xcopy /f /y "C:\Downloads\Rifbot-Project.zip" "%~dp0Rifbot-Project.zip"
